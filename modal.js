d29af606cf08f027551b4d5a1c4048d455592cc3
wissal senussi <wissal.senussi@gmail.com>
2022-04-19 22:16:39 +0200

    FORM validation

--- modal.js
+++ modal.js
@@ -37,7 +37,9 @@ modalClose2.forEach((close) => close.addEventListener('click', closeModal2))
 
 function closeModal1() {
     modalbg.style.display = 'none'
+    modalBgConfirm.style.display = 'none'
 }
+
 
 function closeModal2() {
     modalBgConfirm.style.display = 'none'
@@ -45,6 +47,8 @@ function closeModal2() {
 
 
 function checkform(type, idelement, iderror) {
+
+
     const regex = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/
     var item = document.getElementById(idelement)
     if (type === "text") {
@@ -66,11 +70,38 @@ function checkform(type, idelement, iderror) {
             return ""
         }
 
+    } else if (type === "radio") {
+        const radioBtn = document.getElementsByName("location")
+        let select;
+        for (let btn of radioBtn) {
+            if (btn.checked) {
+                select = btn.value
+                break;
+            }
+        }
+        if (select != undefined) {
+            document.getElementById(iderror).style.display = "none"
+
+            return select
+        } else {
+            document.getElementById(iderror).style.display = "block"
+            return ""
+        }
+
+
+
+    } else if (type === "checkbox") {
+        if (!item.checked) {
+            document.getElementById(iderror).style.display = "block"
+            return ""
+        }
     }
     document.getElementById(iderror).style.display = "none"
     item.style.borderColor = "white"
     return item.value
 }
+
+
 
 // #TODO 2
 function validate(event) {
@@ -81,7 +112,21 @@ function validate(event) {
     var email = checkform("email", "email", "emailError")
     var birthday = checkform("date", "birthday", "birthdayError")
     var quantity = checkform("date", "quantity", "quantityError")
+    var location = checkform("radio", "location", "locationError")
+    var accept = checkform("checkbox", "checkbox1", "acceptError")
 
+    if (firstName != "" && lastName != "" && email != "" && birthday != "" && quantity != "" && location != "" && accept != "") {
+        confirm();
+    }
+
+}
+
+function confirm() {
+
+    const modalBodyForm = document.querySelector('.bground.modal-form')
+    const modalBodyConfirm = document.querySelector('.bground.modal-confirm')
+    modalBodyForm.style.display = 'none'
+    modalBodyConfirm.style.display = 'block'
 
 
 }
